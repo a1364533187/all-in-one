5b711a7d2e584c77643f1cf1b530370ca42664dd
suzhiwu <suzhiwu@kuaishou.com>
2022-01-07 10:19:50 +0800

    add min sum

---
+++ src/main/java/com/bigcow/cn/code/dp/MinPathSum.java
@@ -0,0 +1,29 @@
+package com.bigcow.cn.code.dp;
+
+public class MinPathSum {
+
+    // 1 <= m, n <= 200
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        return minPathSum(grid, m, n);
+    }
+
+    public int minPathSum(int[][] grid, int m, int n) {
+        int[][] minSums = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    minSums[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    minSums[i][j] = minSums[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    minSums[i][j] = minSums[i - 1][j] + grid[i][j];
+                } else {
+                    minSums[i][j] = Math.min(minSums[i - 1][j], minSums[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return minSums[m - 1][n - 1];
+    }
+}
