38cf0642b0a7cc66c2c4da239b591b895511ad9c
suzhiwu <suzhiwu@kuaishou.com>
2022-02-01 17:22:47 +0800

    ac

--- src/main/java/com/bigcow/cn/hot100/CoinChange322Solution.java
+++ src/main/java/com/bigcow/cn/hot100/CoinChange322Solution.java
@@ -1,2 +1,30 @@
-package com.bigcow.cn.hot100;public class CoinChange322Solution {
+package com.bigcow.cn.hot100;
+
+public class CoinChange322Solution {
+
+    public static void main(String[] args) {
+        int[] coins = { 2 };
+        System.out.println(coinChange(coins, 3));
+
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            int curMin = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != -1) {
+                    curMin = Math.min(curMin, dp[i - coin] + 1);
+                }
+            }
+            if (curMin == Integer.MAX_VALUE) {
+                dp[i] = -1;
+            } else {
+                dp[i] = curMin;
+            }
+        }
+        return dp[amount];
+    }
+
 }
