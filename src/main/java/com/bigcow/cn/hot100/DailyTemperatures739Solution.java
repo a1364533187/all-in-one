ff1fc93ce3f10a75a6a519f1b2fda545d1438143
suzhiwu <suzhiwu@kuaishou.com>
2022-02-02 21:23:08 +0800

    add DailyTemperatures739Solution

--- src/main/java/com/bigcow/cn/hot100/DailyTemperatures739Solution.java
+++ src/main/java/com/bigcow/cn/hot100/DailyTemperatures739Solution.java
@@ -1,2 +1,28 @@
-package com.bigcow.cn.hot100;public class DailyTemperatures739Solution {
+package com.bigcow.cn.hot100;
+
+import java.util.Stack;
+
+public class DailyTemperatures739Solution {
+
+    public static void main(String[] args) {
+        int[] temp = {73,74,75,71,69,72,76,73};
+        int[] res = dailyTemperatures(temp);
+    }
+
+    // 单调递减栈 nb， 参考连接：https://leetcode-cn.com/problems/daily-temperatures/solution/leetcode-tu-jie-739mei-ri-wen-du-by-misterbooo/
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            // 保持栈是单调递减栈
+            while (!stack.isEmpty() && temperatures[i] > stack.peek()[1]) {
+                // 栈里的元素需要出栈
+                int[] e = stack.pop();
+                res[e[0]] = i-e[0];
+            }
+            stack.push(new int[]{i, temperatures[i]});
+        }
+
+        return res;
+    }
 }
