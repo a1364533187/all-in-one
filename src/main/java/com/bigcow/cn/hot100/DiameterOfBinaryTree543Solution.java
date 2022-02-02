dcf15f80f92eec23fefc2b3bf34fb1fff6042090
suzhiwu <suzhiwu@kuaishou.com>
2022-02-02 21:20:05 +0800

    add binary tree

--- src/main/java/com/bigcow/cn/hot100/DiameterOfBinaryTree543Solution.java
+++ src/main/java/com/bigcow/cn/hot100/DiameterOfBinaryTree543Solution.java
@@ -1,2 +1,41 @@
-package com.bigcow.cn.hot100;public class DiameterOfBinaryTree543Solution {
+package com.bigcow.cn.hot100;
+
+public class DiameterOfBinaryTree543Solution {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        return len(root.left) + len(root.right);
+    }
+
+    // 计算数的深度
+    public int len(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        int left = len(root.left) + 1;
+        int right = len(root.right) + 1;
+        return Math.max(left, right);
+    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
