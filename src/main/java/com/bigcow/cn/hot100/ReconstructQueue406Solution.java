3b528428cca155e2fe45a7c947c87592eeb65e5b
suzhiwu <suzhiwu@kuaishou.com>
2022-02-05 17:13:06 +0800

    reconstructQueue

--- src/main/java/com/bigcow/cn/hot100/ReconstructQueue406Solution.java
+++ src/main/java/com/bigcow/cn/hot100/ReconstructQueue406Solution.java
@@ -1,2 +1,38 @@
-package com.bigcow.cn.hot100;public class ReconstructQueue406Solution {
+package com.bigcow.cn.hot100;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReconstructQueue406Solution {
+
+    /**
+     * 先排序，后插入list
+     * https://leetcode-cn.com/problems/queue-reconstruction-by-height/solution/406-gen-ju-shen-gao-zhong-jian-dui-lie-java-xian-p/
+     * @param people
+     * @return
+     */
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o2[0] - o1[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+        List<int[]> resList = new LinkedList<>();
+        for (int i = 0; i < people.length; i++) {
+            resList.add(people[i][1], people[i]);
+        }
+        int[][] res = new int[people.length][2];
+        for (int i = 0; i < resList.size(); i++) {
+            res[i] = resList.get(i);
+        }
+        return res;
+    }
 }
