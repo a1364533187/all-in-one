4edb97c0259608a31b51fbefa5afe2861b25b018
suzhiwu <suzhiwu@kuaishou.com>
2022-02-06 18:31:34 +0800

    add count

--- src/main/java/com/bigcow/cn/hot100/FindTargetSumWays494Solution.java
+++ src/main/java/com/bigcow/cn/hot100/FindTargetSumWays494Solution.java
@@ -1,2 +1,26 @@
-package com.bigcow.cn.hot100;public class findTargetSumWays {
+package com.bigcow.cn.hot100;
+
+public class FindTargetSumWays494Solution {
+
+    public static void main(String[] args) {
+        int[] nums = { 1 };
+        System.out.println(findTargetSumWays(nums, 1));
+    }
+
+    public static int findTargetSumWays(int[] nums, int target) {
+        int[] count = new int[1];
+        DFS(nums, target, 0, count);
+        return count[0];
+    }
+
+    private static void DFS(int[] nums, int target, int index, int[] count) {
+        if (index >= nums.length) {
+            if (index == nums.length && target == 0) {
+                count[0]++;
+            }
+            return;
+        }
+        DFS(nums, target + nums[index], index + 1, count);
+        DFS(nums, target - nums[index], index + 1, count);
+    }
 }
