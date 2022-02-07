0347ec671fae0704c8a3d0bbd54a5f82b25d5a05
suzhiwu <suzhiwu@kuaishou.com>
2022-02-07 15:47:09 +0800

    convert bst

--- src/main/java/com/bigcow/cn/code/tree/tree/ConvertBstToGreaterTree538Solution.java
+++ src/main/java/com/bigcow/cn/code/tree/tree/ConvertBstToGreaterTree538Solution.java
@@ -2,22 +2,52 @@ package com.bigcow.cn.code.tree.tree;
 
 public class ConvertBstToGreaterTree538Solution {
 
-    int sum = 0;
-
     public TreeNode convertBST(TreeNode root) {
-        traversal(root);
+        int[] sum = new int[1];
+        traversal(root, sum);
         return root;
     }
 
-    // 中序遍历 左中右， 
-    // 反中序遍历： 右中左 ， 本题的核心
-    public void traversal(TreeNode root) {
+    public TreeNode traversal(TreeNode root, int[] sum) {
         if (root == null) {
-            return;
+            return root;
         }
-        traversal(root.right);
-        root.val = sum + root.val;
-        sum = root.val;
-        traversal(root.left);
+        traversal(root.right, sum);
+        root.val = root.val + sum[0];
+        sum[0] = root.val;
+        traversal(root.left, sum);
+        return root;
     }
+
+    //    int sum = 0;
+    //
+    //    public TreeNode convertBST(TreeNode root) {
+    //        if (root == null) {
+    //            return root;
+    //        }
+    //        convertBST(root.right);
+    //        root.val = root.val + sum;
+    //        sum = root.val;
+    //        convertBST(root.left);
+    //        return root;
+    //    }
+
+    //    int sum = 0;
+
+    //    public TreeNode convertBST(TreeNode root) {
+    //        traversal(root);
+    //        return root;
+    //    }
+
+    //    // 中序遍历 左中右， 
+    //    // 反中序遍历： 右中左 ， 本题的核心
+    //    public void traversal(TreeNode root) {
+    //        if (root == null) {
+    //            return;
+    //        }
+    //        traversal(root.right);
+    //        root.val = sum + root.val;
+    //        sum = root.val;
+    //        traversal(root.left);
+    //    }
 }
---
+++ src/main/java/com/bigcow/cn/code/tree/tree/DiameterOfBinaryTree543Solution.java
@@ -0,0 +1,2 @@
+package com.bigcow.cn.code.tree.tree;public class DiameterOfBinaryTree543Solution {
+}
