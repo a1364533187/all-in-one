8ed1fa20cdaf929265ca6e614fb924254da9a83b
suzhiwu <suzhiwu@kuaishou.com>
2022-02-07 22:28:19 +0800

    add path sum III

--- src/main/java/com/bigcow/cn/hot100/PathSumIII437Solution1.java
+++ src/main/java/com/bigcow/cn/hot100/PathSumIII437Solution1.java
@@ -26,7 +26,7 @@ public class PathSumIII437Solution1 {
         if (root == null) {
             return;
         }
-        System.out.println(root.val);
+        System.out.println(root.val); // 这里不会影响到左右子树的结果
         traversal(root.left);
         traversal(root.right);
     }
@@ -45,6 +45,8 @@ public class PathSumIII437Solution1 {
         calCountTraversal(root.left, targetSum, count, freqPreSumMap);
         calCountTraversal(root.right, targetSum, count, freqPreSumMap);
         freqPreSumMap.put(root.val, freqPreSumMap.getOrDefault(root.val, 0) - 1); // Map 需要撤销操作？
+        // 状态恢复代码的作用就是： 在遍历完一个节点的所有子节点后，将其从map中除去。否则左子树的结果会影响到右子树的结果
+        // 见leetcode: https://leetcode-cn.com/problems/path-sum-iii/solution/dui-qian-zhui-he-jie-fa-de-yi-dian-jie-s-dey6/
     }
 
     // 第一步，算前缀和, 先序遍历
@@ -53,8 +55,9 @@ public class PathSumIII437Solution1 {
             return;
         }
         if (root.left != null) {
-            root.left.val = root.val + root.left.val; // 不需要撤销操作？
+            root.left.val = root.val + root.left.val; // 不需要撤销操作， 原因是这里左子树的结果， 并不会影响到右子树
             traversalPreSum(root.left);
+//            root.left.val =  root.left.val - root.val;  原因是root 和 root.left 是不相关的2层
         }
         if (root.right != null) {
             root.right.val = root.val + root.right.val;
