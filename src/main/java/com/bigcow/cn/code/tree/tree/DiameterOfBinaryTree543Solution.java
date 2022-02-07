12c693af829e9f6fe5bace1938436915e7aa8490
suzhiwu <suzhiwu@kuaishou.com>
2022-02-07 17:41:00 +0800

    add root path

--- src/main/java/com/bigcow/cn/code/tree/tree/DiameterOfBinaryTree543Solution.java
+++ src/main/java/com/bigcow/cn/code/tree/tree/DiameterOfBinaryTree543Solution.java
@@ -1,2 +1,33 @@
-package com.bigcow.cn.code.tree.tree;public class DiameterOfBinaryTree543Solution {
+package com.bigcow.cn.code.tree.tree;
+
+public class DiameterOfBinaryTree543Solution {
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] res = traversal(root);
+        return res[0];
+    }
+
+    /**
+     * int[] int[0] 存放的是截止到当前节点的最大路径(不一定包含当前节点)
+     * int[1] 存放的是当前节点到左子树或者右子树最大的路径
+     * @param root
+     * @return
+     */
+    public int[] traversal(TreeNode root) {
+        if (root == null) {
+            return new int[] { 0, 0 };
+        }
+        int[] left = new int[] { 0, 0 };
+        if (root.left != null) {
+            left = traversal(root.left);
+            left[1] += 1;
+        }
+        int[] right = new int[] { 0, 0 };
+        if (root.right != null) {
+            right = traversal(root.right);
+            right[1] += 1;
+        }
+        int max = Math.max(Math.max(left[0], left[1] + right[1]), right[0]);
+        return new int[] { max, Math.max(left[1], right[1]) };
+    }
 }
