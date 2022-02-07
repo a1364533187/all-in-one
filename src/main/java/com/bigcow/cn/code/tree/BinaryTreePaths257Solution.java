73141b1361985c3bc2e49ca675382b81ad02c782
suzhiwu <suzhiwu@kuaishou.com>
2022-02-07 15:48:11 +0800

    add pat

--- src/main/java/com/bigcow/cn/code/tree/BinaryTreePaths257Solution.java
+++ src/main/java/com/bigcow/cn/code/tree/BinaryTreePaths257Solution.java
@@ -1,2 +1,46 @@
-package com.bigcow.cn.code.tree;public class BinaryTreePaths257Solution {
+package com.bigcow.cn.code.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths257Solution {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        backtrace(root, root.val + "", res);
+        return res;
+    }
+
+    private void backtrace(TreeNode root, String path, List<String> res) {
+        if (root.left == null && root.right == null) {
+            res.add(path);
+            return;
+        }
+        if (root.left != null) {
+            backtrace(root.left, path + "->" + root.left.val, res);
+        }
+        if (root.right != null) {
+            backtrace(root.right, path + "->" + root.right.val, res);
+        }
+    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
