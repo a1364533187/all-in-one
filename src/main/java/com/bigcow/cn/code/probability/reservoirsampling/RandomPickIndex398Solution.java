2378b1db80d00eac10818d22735409608821b8f1
suzhiwu <suzhiwu@kuaishou.com>
2022-02-10 17:01:41 +0800

    add r

--- src/main/java/com/bigcow/cn/code/probability/reservoirsampling/RandomPickIndex398Solution.java
+++ src/main/java/com/bigcow/cn/code/probability/reservoirsampling/RandomPickIndex398Solution.java
@@ -1,2 +1,26 @@
-package com.bigcow.cn.code.probability.reservoirsampling;public class RandomPickIndex398Solution {
+package com.bigcow.cn.code.probability.reservoirsampling;
+
+import java.util.Random;
+
+public class RandomPickIndex398Solution {
+
+    private int[] nums;
+
+    public RandomPickIndex398Solution(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int pick(int target) {
+        Random random = new Random();
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                n++;
+                if (random.nextInt(n) == 0) {
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
 }
