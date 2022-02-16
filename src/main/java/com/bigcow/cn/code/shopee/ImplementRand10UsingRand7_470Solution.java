284940b6f1159fbf55d665d784fe19f199370a65
suzhiwu <suzhiwu@kuaishou.com>
2022-02-16 21:36:10 +0800

    add Rand7

--- src/main/java/com/bigcow/cn/code/shopee/ImplementRand10UsingRand7_470Solution.java
+++ src/main/java/com/bigcow/cn/code/shopee/ImplementRand10UsingRand7_470Solution.java
@@ -1,2 +1,28 @@
-package com.bigcow.cn.code.shopee;public class ImplementRand10UsingRand7_470Solution {
+package com.bigcow.cn.code.shopee;
+
+import java.util.Random;
+
+public class ImplementRand10UsingRand7_470Solution extends SolBase {
+
+    /**
+     * https://note.youdao.com/web/#/file/recent/note/caf437f6c3640cda9b69e4967bb02c53/
+     * @return
+     */
+    public int rand10() {
+        while (true) {
+            int val = (rand7()-1)*7 + rand7();
+            if (val <= 10) {
+                return val;
+            }
+        }
+    }
+}
+
+class SolBase {
+
+    public int rand7() {
+        Random r = new Random();
+        int val = r.nextInt(7);
+        return val;
+    };
 }
