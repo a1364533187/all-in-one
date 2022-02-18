2ad960e60e5d8fb8cc0c8ecccfa5d38bf6e122de
suzhiwu <suzhiwu@kuaishou.com>
2022-02-18 09:34:31 +0800

    add sub

--- src/main/java/com/bigcow/cn/code/shopee/LongestSubstringWithAtMostTwoDistinctCharacters159Solution.java
+++ src/main/java/com/bigcow/cn/code/shopee/LongestSubstringWithAtMostTwoDistinctCharacters159Solution.java
@@ -1,2 +1,38 @@
-package com.bigcow.cn.code.shopee;public class LongestSubstringWithAtMostTwoDistinctCharacters159Solution {
+package com.bigcow.cn.code.shopee;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithAtMostTwoDistinctCharacters159Solution {
+
+    public static void main(String[] args) {
+        String s = "ccbbbaaa";
+        System.out.println(lengthOfLongestSubstringTwoDistinct(s));
+    }
+
+    /**
+     * 滑动窗 + Hashamp
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstringTwoDistinct(String s) {
+        HashMap<Character, Integer> charIndexMap = new HashMap<>();
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            charIndexMap.put(c, end);
+            while (charIndexMap.size() > 2) { //shrink
+                char cc = s.charAt(start);
+                int index = charIndexMap.get(cc);
+                if (index == start) {
+                    charIndexMap.remove(cc);
+                }
+                start++;
+            }
+            max = Math.max(end - start + 1, max);
+            end++;
+        }
+        return Math.max(end - start, max);
+    }
 }
