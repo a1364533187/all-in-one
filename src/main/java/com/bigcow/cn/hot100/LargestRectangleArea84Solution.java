c543024f9700c30bf8acd7517fc62450dd0a95b4
suzhiwu <suzhiwu@kuaishou.com>
2022-02-25 15:29:35 +0800

    add LargestRectangleArea84Solution

--- src/main/java/com/bigcow/cn/hot100/LargestRectangleArea84Solution.java
+++ src/main/java/com/bigcow/cn/hot100/LargestRectangleArea84Solution.java
@@ -1,25 +1,24 @@
 package com.bigcow.cn.hot100;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 public class LargestRectangleArea84Solution {
 
     public static void main(String[] args) {
-        int[] heights = { 2, 1,2 };
+        int[] heights = { 2, 1, 2 };
         System.out.println(largestRectangleArea(heights));
     }
 
     // 单调栈的解法，妙啊
     // https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/84-by-ikaruga/
+    // bilibili的视频： https://www.bilibili.com/video/BV1jV411S7WJ?p=1&share_medium=iphone&share_plat=ios&share_session_id=298FAEF7-1C0B-4AF9-B39C-0D41C6A93EA2&share_source=WEIXIN&share_tag=s_i&timestamp=1645769733&unique_k=1B8wEaR
     public static int largestRectangleArea(int[] heights) {
         int res = 0;
         Stack<Integer> stack = new Stack<>();
         int[] new_heights = new int[heights.length + 2];
-        for (int i = 1; i < heights.length + 1; i++) new_heights[i] = heights[i - 1];
-        System.out.println(Arrays.toString(new_heights));
+        for (int i = 1; i < heights.length + 1; i++)
+            new_heights[i] = heights[i - 1];
         for (int i = 0; i < new_heights.length; i++) {
-            //System.out.println(stack.toString());
             while (!stack.isEmpty() && new_heights[stack.peek()] > new_heights[i]) {
                 int cur = stack.pop();
                 res = Math.max(res, (i - stack.peek() - 1) * new_heights[cur]);
