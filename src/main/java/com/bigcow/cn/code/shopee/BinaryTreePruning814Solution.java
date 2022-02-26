194d9b16cf61864ece110989ad7b02906544c760
suzhiwu <suzhiwu@kuaishou.com>
2022-02-26 11:20:23 +0800

    add BinaryTreePruning814Solution

--- src/main/java/com/bigcow/cn/code/shopee/BinaryTreePruning814Solution.java
+++ src/main/java/com/bigcow/cn/code/shopee/BinaryTreePruning814Solution.java
@@ -8,6 +8,7 @@ public class BinaryTreePruning814Solution {
         }
         TreeNode left = pruneTree(root.left);
         TreeNode right = pruneTree(root.right);
+        // 这里依赖的是后续遍历返回的结果， 所以是left 和 right 而不是root.left 和 root.right
         if (root.val == 0 && left == null && right == null) {
             return null;
         }
