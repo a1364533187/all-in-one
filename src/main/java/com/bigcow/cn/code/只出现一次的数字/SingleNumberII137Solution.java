a055fff57109d7331bc28162678cc6cb6b5b2c12
suzhiwu <suzhiwu@kuaishou.com>
2022-02-27 19:53:28 +0800

    add single num

--- src/main/java/com/bigcow/cn/code/只出现一次的数字/SingleNumberII137Solution.java
+++ src/main/java/com/bigcow/cn/code/只出现一次的数字/SingleNumberII137Solution.java
@@ -1,2 +1,26 @@
-package com.bigcow.cn.code.只出现一次的数字;public class SingleNumberII137Solution {
+package com.bigcow.cn.code.只出现一次的数字;
+
+import java.util.HashSet;
+
+public class SingleNumberII137Solution {
+
+    /**
+     * 求和 + hashset
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        long setSum = 0;
+        long numsSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            numsSum += nums[i];
+            if (set.contains(nums[i])) {
+                continue;
+            }
+            setSum += 1.0 * nums[i] * 3;
+            set.add(nums[i]);
+        }
+        return (int) ((setSum - numsSum) / 2);
+    }
 }
