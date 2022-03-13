f24c40613558149ea213e4b4c21ececbdf08da0f
suzhiwu <suzhiwu@kuaishou.com>
2022-03-13 18:47:56 +0800

    add 01 package

--- src/main/java/com/bigcow/cn/code/零钱兑换/CoinChange2Solution.java
+++ src/main/java/com/bigcow/cn/code/零钱兑换/CoinChange2Solution.java
@@ -1,2 +1,22 @@
-package com.bigcow.cn.code.零钱兑换;public class CoinChange2Solution {
+package com.bigcow.cn.code.零钱兑换;
+
+public class CoinChange2Solution {
+
+    public static void main(String[] args) {
+        int[] coins = { 1, 2, 5 };
+        System.out.println(change(5, coins));
+    }
+
+    public static int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = 1; i <= amount; i++) { //这个硬币选0个， 选1个，选2个, 最多选择amount 个
+                if (i - coin >= 0) {
+                    dp[i] = dp[i] + dp[i - coin];
+                }
+            }
+        }
+        return dp[amount];
+    }
 }
